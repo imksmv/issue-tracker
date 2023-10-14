54cf4a10084191d7721092f0f84d32a9b92ee572
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-10-14 18:15:15 +0100

    Add additional loading skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,22 @@
+import { Card } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
 import React from "react"
 
 const LoadingIssueDetailPage = () => {
-  return <div>Loading...</div>
+  return (
+    <div className="container">
+      <Skeleton className="h-5 w-[170px] mb-5" />
+      <div className="flex space-x-3">
+        <Skeleton className="h-5 w-[70px]" />
+        <Skeleton className="h-5 w-[200px]" />
+      </div>
+      <Card className="prose dark:prose-invert p-5 mt-5 space-y-3">
+        <Skeleton className="h-5" />
+        <Skeleton className="h-5" />
+        <Skeleton className="h-5" />
+      </Card>
+    </div>
+  )
 }
 
 export default LoadingIssueDetailPage
--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,5 +1,12 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>
+  return (
+    <div className="container space-y-3">
+      <Skeleton className="h-10" />
+      <Skeleton className="h-80" />
+    </div>
+  )
 }
 
 export default LoadingNewIssuePage
