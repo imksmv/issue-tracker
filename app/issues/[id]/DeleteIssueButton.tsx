1f8830927b870f03c40d8de32159d89e19db7834
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-10-15 12:10:40 +0100

    Show a confirmation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,12 +1,38 @@
+"use client"
+
 import { Button } from "@/components/ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
 import { Trash2 } from "lucide-react"
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button variant="destructive">
-      <Trash2 className="mr-2 h-4 w-4" />
-      Delete Issue
-    </Button>
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="destructive">
+          <Trash2 className="mr-2 h-4 w-4" />
+          Delete Issue
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Confirm Deletion?</DialogTitle>
+          <DialogDescription>This action cannot be undone.</DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <Button type="submit" variant="destructive">
+            Delete Issue
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   )
 }
 
