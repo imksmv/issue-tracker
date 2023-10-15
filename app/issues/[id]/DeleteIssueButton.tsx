afb0370ab17660c095e8c25c9b9ccd049477cf13
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-10-15 12:52:06 +0100

    Handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -10,12 +10,28 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog"
+import { useToast } from "@/components/ui/use-toast"
 import axios from "axios"
 import { Trash2 } from "lucide-react"
 import { useRouter } from "next/navigation"
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter()
+  const { toast } = useToast()
+
+  const deleteIssue = async () => {
+    try {
+      await axios.delete("/api/issues/" + issueId)
+      router.push("/issues")
+      router.refresh()
+    } catch (error) {
+      toast({
+        variant: "destructive",
+        title: "Uh oh! Something went wrong.",
+        description: "There was a problem with your request.",
+      })
+    }
+  }
 
   return (
     <Dialog>
@@ -31,15 +47,7 @@ const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
           <DialogDescription>This action cannot be undone.</DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button
-            type="submit"
-            variant="destructive"
-            onClick={async () => {
-              await axios.delete("/api/issues/" + issueId)
-              router.push("/issues")
-              router.refresh()
-            }}
-          >
+          <Button variant="destructive" onClick={deleteIssue}>
             Delete Issue
           </Button>
         </DialogFooter>
