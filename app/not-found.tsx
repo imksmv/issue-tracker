ec11bd54ee09c296a08e41004adac6121a60c9d8
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-10-17 17:33:02 +0100

    Create custom not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import notFoundDark from "@/public/not-found-dark.svg"
+import notFoundLight from "@/public/not-found-light.svg"
+import { useTheme } from "next-themes"
+import Image from "next/image"
+
+const NotFound = () => {
+  const { theme } = useTheme()
+  const notFoundSvgColor = theme === "dark" ? notFoundLight : notFoundDark
+
+  return (
+    <div className="grid place-content-center h-[calc(100vh-73px)]">
+      <Image
+        className="lg:h-[600px] lg:w-[600px]"
+        src={notFoundSvgColor}
+        alt="not-found"
+      />
+    </div>
+  )
+}
+
+export default NotFound
