4a6036c15f23dc79c84f01e9cc675dfe944de548
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-10-18 11:29:45 +0100

    Refactor: Extract issue table

---
+++ app/issues/IssueTable.tsx
@@ -0,0 +1,93 @@
+import IssueStatusBadge from "@/components/IssueStatusBadge"
+import Link from "@/components/Link"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+import { Issue, Status } from "@prisma/client"
+import { ArrowDownWideNarrow } from "lucide-react"
+import NextLink from "next/link"
+
+export interface IssueQuery {
+  status: Status
+  orderBy: keyof Issue
+  page: string
+}
+
+interface Props {
+  searchParams: IssueQuery
+  issues: Issue[]
+}
+
+const IssueTable = ({ searchParams, issues }: Props) => {
+  return (
+    <Table className="mb-3">
+      <TableCaption>A list of your recent issues.</TableCaption>
+      <TableHeader>
+        <TableRow>
+          {columns.map((column) => (
+            <TableHead key={column.value} className={column.className}>
+              <div className="flex items-center gap-1 w-24">
+                <NextLink
+                  href={{
+                    query: { ...searchParams, orderBy: column.value },
+                  }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && (
+                  <ArrowDownWideNarrow size={14} />
+                )}
+              </div>
+            </TableHead>
+          ))}
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {issues.map((issue) => (
+          <TableRow key={issue.id}>
+            <TableCell className="font-medium">
+              <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
+              <div className="block md:hidden mt-1">
+                <IssueStatusBadge status={issue.status} />
+              </div>
+            </TableCell>
+            <TableCell className="hidden md:table-cell">
+              <IssueStatusBadge status={issue.status} />
+            </TableCell>
+            <TableCell className="hidden md:table-cell">
+              {issue.createdAt.toDateString()}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  )
+}
+
+const columns: {
+  label: string
+  value: keyof Issue
+  className?: string
+}[] = [
+  { label: "Issue", value: "title" },
+  {
+    label: "Status",
+    value: "status",
+    className: "hidden md:table-cell",
+  },
+  {
+    label: "Created",
+    value: "createdAt",
+    className: "hidden md:table-cell",
+  },
+]
+
+export const columnNames = columns.map((column) => column.value)
+
+export default IssueTable
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,32 +1,14 @@
-import IssueStatusBadge from "@/components/IssueStatusBadge"
-import Link from "@/components/Link"
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table"
-import prisma from "@/prisma/client"
-import { Issue, Status } from "@prisma/client"
-import { ArrowDownWideNarrow } from "lucide-react"
-import NextLink from "next/link"
-import IssueActions from "./IssueActions"
 import Pagination from "@/components/Pagination"
+import prisma from "@/prisma/client"
+import { Status } from "@prisma/client"
+import IssueActions from "./IssueActions"
+import IssueTable, { IssueQuery, columnNames } from "./IssueTable"
 
 interface Props {
-  searchParams: { status: Status; orderBy: keyof Issue; page: string }
+  searchParams: IssueQuery
 }
 
 const IssuesPage = async ({ searchParams }: Props) => {
-  const columns: { label: string; value: keyof Issue; className?: string }[] = [
-    { label: "Issue", value: "title" },
-    { label: "Status", value: "status", className: "hidden md:table-cell" },
-    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
-  ]
-
   const statuses = Object.values(Status)
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
@@ -34,9 +16,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
 
   const where = { status }
 
-  const orderBy = columns
-    .map((column) => column.value)
-    .includes(searchParams.orderBy)
+  const orderBy = columnNames.includes(searchParams.orderBy)
     ? { [searchParams.orderBy]: "asc" }
     : undefined
 
@@ -55,7 +35,8 @@ const IssuesPage = async ({ searchParams }: Props) => {
   return (
     <section className="container">
       <IssueActions />
-      <div className="flex justify-center">
+
+      <div className="ml-4">
         <Pagination
           itemCount={issueCount}
           currentPage={page}
@@ -63,47 +44,7 @@ const IssuesPage = async ({ searchParams }: Props) => {
         />
       </div>
 
-      <Table className="mb-3">
-        <TableCaption>A list of your recent issues.</TableCaption>
-        <TableHeader>
-          <TableRow>
-            {columns.map((column) => (
-              <TableHead key={column.value} className={column.className}>
-                <div className="flex items-center gap-1 w-24">
-                  <NextLink
-                    href={{
-                      query: { ...searchParams, orderBy: column.value },
-                    }}
-                  >
-                    {column.label}
-                  </NextLink>
-                  {column.value === searchParams.orderBy && (
-                    <ArrowDownWideNarrow size={14} />
-                  )}
-                </div>
-              </TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {issues.map((issue) => (
-            <TableRow key={issue.id}>
-              <TableCell className="font-medium">
-                <Link href={`/issues/${issue.id}`}>{issue.title}</Link>
-                <div className="block md:hidden mt-1">
-                  <IssueStatusBadge status={issue.status} />
-                </div>
-              </TableCell>
-              <TableCell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
-              </TableCell>
-              <TableCell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <IssueTable searchParams={searchParams} issues={issues} />
     </section>
   )
 }
