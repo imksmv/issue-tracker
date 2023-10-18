436b7842f7688b8cf1b67a0c1a3712d03cfb7d0f
Illia Maksymov <98290303+imksmv@users.noreply.github.com>
2023-10-18 12:44:31 +0100

    Build the issue summary component

---
+++ app/IssueSummary.tsx
@@ -0,0 +1,40 @@
+import { TypographyP } from "@/components/TypographyP"
+import { Card } from "@/components/ui/card"
+import { Status } from "@prisma/client"
+import Link from "next/link"
+
+interface Props {
+  open: number
+  inProgress: number
+  closed: number
+}
+
+const IssueSummary = ({ open, inProgress, closed }: Props) => {
+  const containers: { label: string; value: number; status: Status }[] = [
+    { label: "Open Issues", value: open, status: "OPEN" },
+    { label: "In-progress Issues", value: inProgress, status: "IN_PROGRESS" },
+    { label: "Closed Issues", value: closed, status: "CLOSED" },
+  ]
+
+  return (
+    <div className="flex gap-4">
+      {containers.map((container) => (
+        <Link href={`/issues/?status=${container.status}`}>
+          <Card
+            className="hover:shadow-lg hover:scale-105 transition-all"
+            key={container.label}
+          >
+            <div className="flex flex-col gap-2 p-3">
+              {container.label}
+              <TypographyP className="text-2xl font-bold">
+                {container.value}
+              </TypographyP>
+            </div>
+          </Card>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default IssueSummary
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import prisma from "@/prisma/client"
+import IssueSummary from "./IssueSummary"
 import LatestIssues from "./LatestIssues"
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.issue.count({ where: { status: "OPEN" } })
+  const closed = await prisma.issue.count({ where: { status: "CLOSED" } })
+  const inProgress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  })
+
   return (
     <section className="container">
-      <LatestIssues />
+      {/* <LatestIssues /> */}
+      <IssueSummary open={open} inProgress={inProgress} closed={closed} />
     </section>
   )
 }
